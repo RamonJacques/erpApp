e3492c2399fea8109f7ac171b6cd90a427a66344
RamonJacques <ramonjacquesrs@gmail.com>
2024-01-14 15:33:16 -0300

    Pckq Utils (alerts, constraints and tryParse)

---
+++ src/main/java/com/consafe/erpApp/gui/utils/Utils.java
@@ -0,0 +1,28 @@
+package com.consafe.erpApp.gui.utils;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+}
